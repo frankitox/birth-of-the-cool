a112a1bde0e8a54eac4e9df9739749e1832cec25
franquito <franconbiasin@gmail.com>
2016-03-21 22:11:26 -0300

    Added uni logos

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 rm -rf /var/www/html/weblcc/themes/birthofcool/
 cp -r stylesheets /var/www/html/weblcc/themes/birthofcool
+chmod -R go+rx /var/www/html/weblcc/themes/birthofcool
--- stylesheets/templates/uni-logos.tpl.php
+++ stylesheets/templates/uni-logos.tpl.php
@@ -1,6 +1,6 @@
-<img src="<?= image_url ('logo-unr.png') ?>"
-     id="logo-unr"
-     alt="Logo UNR">
+<img src="<?= image_url ('logo-fceia.png') ?>"
+     id="fceia-unr"
+     alt="Logo FCEIA">
 <img src="<?= image_url ('logo-unr.png') ?>"
      id="logo-unr"
      class="last-logo"
