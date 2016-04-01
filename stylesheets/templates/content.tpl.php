33e166a030488a49f74044cbf92c3b50693f1a92
franquito <franconbiasin@gmail.com>
2016-03-31 22:31:45 -0300

    Moved section sidebar_second to a proper place

---
+++ stylesheets/templates/content.tpl.php
@@ -0,0 +1,31 @@
+<div class="section-wrapper">
+  <div class="section">
+    <?php if ($page['highlighted']): ?>
+      <div id="highlighted">
+        <?php print render($page['highlighted']); ?>
+      </div>
+    <?php endif; ?>
+    <a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <?php print render($title_suffix); ?>
+    <?php if ($tabs): ?>
+      <div class="tabs">
+        <?php print render($tabs); ?>
+      </div>
+    <?php endif; ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links">
+        <?php print render($action_links); ?>
+      </ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+    <?php print $feed_icons; ?>
+  </div>
+</div>
+
+<?php if ($page['sidebar_second']): ?>
+<div id="sidebar-second" class="column sidebar">
+  <?php print render($page['sidebar_second']); ?>
+</div> <!-- /.section, /#sidebar-second -->
+<?php endif; ?>
--- stylesheets/templates/page.tpl.php
+++ stylesheets/templates/page.tpl.php
@@ -105,7 +105,11 @@
     
     <div class="content-container
              <?= ($page['sidebar_first']) ?
-                  "sidebar-first-on" : ""; ?>">
+                  "sidebar-first-on" : " "; ?>
+             <?= ($page['sidebar_second']) ?
+                  "sidebar-second-on" : " "; ?>
+                  ">
+      
       <?php if ($page['featured']): ?>
         <div id="featured"><div class="section clearfix">
           <?php print render($page['featured']); ?>
@@ -118,33 +122,10 @@
         </div></div> <!-- /.section, /#sidebar-first -->
       <?php endif; ?>
     
-      <div id="content" class="column"><div class="section">
-        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?>
-          <div class="tabs">
-            <?php print render($tabs); ?>
-          </div>
-        <?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links">
-            <?php print render($action_links); ?>
-          </ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-    </div>
-
-    </div></div> <!-- /.section, /#content -->
-
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_second']); ?>
-      </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
+      <div id="content" class="column">
+        <?php require 'content.tpl.php'; ?>
+      </div>
+    </div><!-- .content-container -->
 
   </div></div> <!-- /#main, /#main-wrapper -->
 
