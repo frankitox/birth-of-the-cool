c93deaac34c11c3d2c2fb1397d4b6d05d494ae7d
franquito <franconbiasin@gmail.com>
2016-03-31 22:00:10 -0300

    First styling over materia

--- stylesheets/templates/node--materia.tpl.php
+++ stylesheets/templates/node--materia.tpl.php
@@ -112,6 +112,12 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
+    ?>
+    <div class="field-group">
+      <?php print render($content['field_docentes']) ?>
+      <?php print render($content['field_horarios']) ?>
+    </div>
+    <?php
       print render($content);
     ?>
   </div>
