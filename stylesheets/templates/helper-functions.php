5e00cfd4e7179d5b6c31ae9b3247321290d7398f
franquito <franconbiasin@gmail.com>
2016-04-15 18:09:53 -0300

    Fixed image_url helper function so it works in production

--- stylesheets/templates/helper-functions.php
+++ stylesheets/templates/helper-functions.php
@@ -4,7 +4,7 @@
  * Wrapper for img src attribute.
  */
 function image_url ($image) {
-  return path_to_theme() . "/images/" . $image;
+  return base_path() . path_to_theme() . "/images/" . $image;¬
 }
 
 ?>
