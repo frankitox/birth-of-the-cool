dd934df066c419208407008f9fca8ce7e157be9d
franquito <franconbiasin@gmail.com>
2016-04-15 19:48:43 -0300

    Fixed little issue, and improved slider view

--- stylesheets/templates/helper-functions.php
+++ stylesheets/templates/helper-functions.php
@@ -3,8 +3,8 @@
 /**
  * Wrapper for img src attribute.
  */
-function image_url ($image) {
-  return base_path() . path_to_theme() . "/images/" . $image;¬
+function image_url($image) {
+  return base_path().path_to_theme()."/images/".$image;
 }
 
 ?>
