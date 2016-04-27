6eb38374409814f9650dc7c387ff72df03aff5fa
franquito <franconbiasin@gmail.com>
2016-04-26 23:08:24 -0300

    Added link to uni logos

--- stylesheets/templates/uni-logos.tpl.php
+++ stylesheets/templates/uni-logos.tpl.php
@@ -1,7 +1,15 @@
-<img src="<?= image_url ('logo-fceia.png') ?>"
-     id="fceia-unr"
-     alt="Logo FCEIA">
-<img src="<?= image_url ('logo-unr.png') ?>"
-     id="logo-unr"
-     class="last-logo"
-     alt="Logo UNR">
+<a href="http://fceia.unr.edu.ar/"
+   title="Facultad de Cs. Exactas y Agrimensura"
+   target="_blank">
+  <img src="<?= image_url ('logo-fceia.png') ?>"
+       id="fceia-unr"
+       alt="Logo FCEIA">
+</a>
+<a href="https://www.unr.edu.ar/"
+   title="Universidad Nacional de Rosario"
+   target="_blank">
+  <img src="<?= image_url ('logo-unr.png') ?>"
+       id="logo-unr"
+       class="last-logo"
+       alt="Logo UNR">
+</a>
