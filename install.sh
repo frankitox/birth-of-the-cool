1bbffb141ff3e5eb308aced8489a5c95c881f3c3
franquito <franconbiasin@gmail.com>
2016-04-28 14:49:30 -0300

    Little changes on install.sh  script

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@
 rm -rf /var/www/html/weblcc/themes/birthofcool/
 cp -r stylesheets /var/www/html/weblcc/themes/birthofcool
 chmod -R go+rx /var/www/html/weblcc/themes/birthofcool
+
+# To install at Kleene:
+# scp -r ~/birthofcool/stylesheets kleene:~
+# ssh kleene
+# chown -R fbiasin:www-data stylesheets
+# find stylesheets/ -type d -exec chmod 770 {} \;
+# find stylesheets/ -type f -exec chmod 660 {} \;
+# rm -rf /var/www/weblcc/themes/birthofcool
+# mv stylesheets /var/www/weblcc/themes/birthofcool
+# echo "Also make sure you cleaned cache"
