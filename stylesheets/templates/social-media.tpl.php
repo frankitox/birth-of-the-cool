bdeaa6843acc7fd71b7db6f4a4624057257d1a96
franquito <franconbiasin@gmail.com>
2016-05-03 23:42:27 -0300

    Added links to social media

--- stylesheets/templates/social-media.tpl.php
+++ stylesheets/templates/social-media.tpl.php
@@ -1,8 +1,8 @@
-<a href="#" target="_blank"
+<a href="https://www.facebook.com/lccrosario" target="_blank"
    title="Facebook">
   <i class="icon-facebook-circled"></i>
 </a>
-<a href="#" target="_blank"
+<a href="https://www.youtube.com/channel/UC-CReVEx4-3AfJOH1Tr-udw" target="_blank"
    title="YouTube">
   <i class="icon-youtube-square"></i>
 </a>
